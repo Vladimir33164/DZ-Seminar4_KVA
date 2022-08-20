9d90f4c9676b321d36a02af104adc627f200bdec
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-20 17:05:47 +0300

    задача27. решение в цикле while

---
+++ Z27(while)/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 27: Напишите программу, которая принимает на 
+//вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+Console.WriteLine("Enter number:");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (i > 0)
+{
+sum = sum + (i % 10);
+i = i / 10;
+}
+Console.WriteLine($"Sum of numbers: {sum}");
