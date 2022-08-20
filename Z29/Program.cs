726c51413a1460539266724a87f673fe1fd3db35
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-20 20:44:41 +0300

    задача29. вывод на экран массива из 8 элементов

--- Z29/Program.cs
+++ Z29/Program.cs
@@ -4,24 +4,39 @@
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 //6, 1, 33 -> [6, 1, 33]
 
-int[] GetArray(int size)//создаем объект, массив типа GetArray.
+/*int[] GetArray(int size)//создаем объект, массив типа GetArray.
 {
     int[]result = new int[size];
     for (int i = 0; i < result.Length; i++)//цикл ввода элементов массива пользователем
         {
-            result[i] = new Random().Next(50);
+            Console.WriteLine("Enter number:");
+            result[i] = Convert.ToInt32(Console.ReadLine());//result[i] = new Random().Next(50);
         }
         return result;
 }    
 int[] testArray = GetArray(8);
 Console.WriteLine($"Array: [{String.Join(", " ,testArray)}]");//вывод значения элемента
-
+*/
 
 //Миша! Очень долго думал, как же решить эту задачу (через ввод массива вручную "Console.WriteLine()")?
 //На семинаре ребята уже подымали этот вопрос, но у меня так ничего и не вышло.
 //Подсказали мне вот такой способ решения этой задачи (внизу). Но уж больно он мне не нравится!!!
 //Я так думаю, тот метод который предлагал ты на семинаре будет проще, но пока не представляю как
 //решить эту задачку твоим методом. Было бы интересно узнать фактическое решение задачи.
+
+int[] GetArray(int size)//создаем объект, массив типа GetArray.
+{
+    int[]result = new int[size];
+    for (int i = 0; i < result.Length; i++)//цикл ввода элементов массива пользователем
+        {
+            Console.WriteLine("Enter number:");
+            result[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        return result;
+}    
+int[] testArray = GetArray(8);
+Console.WriteLine($"Array: [{String.Join(", " ,testArray)}]");//вывод значения элемента
+
 
 /*int Size = 8;//размер массива
 Double[] Massiv = new Double[Size];//создаем объект, массив типа double из 8ми элементов
