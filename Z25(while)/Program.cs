88d85ca2f2cf4487db2f2fb1cdb962c102c4df8f
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-20 16:56:38 +0300

    задача25. решение в цикле while

---
+++ Z25(while)/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A в натуральную 
+//степень B.
+
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+Console.WriteLine("Enter number A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number В");
+int b = Convert.ToInt32(Console.ReadLine());
+int sum = a;
+int i = 1;
+while (i < b)
+{
+    sum = sum * a;
+    i++;
+}
+Console.WriteLine($"Result A to the degree of B: {sum}");
