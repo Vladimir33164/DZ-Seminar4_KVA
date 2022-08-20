8b5de5ff555dee084c73927f76e4583e52e42f4a
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-20 16:51:59 +0300

    задача25. решение в цикле for

---
+++ Z25(for)/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход 
+//два числа (A и B) и возводит число A в натуральную 
+//степень B.
+
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+/*Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число В");
+int b = Convert.ToInt32(Console.ReadLine());
+int sum = a;
+for (int i = 1; i < b; i++)
+{
+    sum = sum * a;
+}
+Console.WriteLine($"Result A to the degree of B: {sum}");
+*/
+
+Console.WriteLine("Enter number А:");//вывод строки
+Double A = Convert.ToDouble(Console.ReadLine());//ввод значения А пользователем
+Console.WriteLine("Enter number В:");//вывод строки
+int B = Convert.ToInt32(Console.ReadLine());//ввод значения В пользователем
+Double Rez = A;
+for (int i = 1; i < B; i++) Rez *= A;//цикл возведения в степень
+Console.WriteLine("Result A to the degree of B:" + Rez.ToString()); //вывод результата
